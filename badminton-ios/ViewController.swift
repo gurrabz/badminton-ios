0818a3e8fd62e086a8801460bcedb2ff069603aa
gurrabz <gustav_m_o_bergstrom@hotmail.com>
2017-06-05 18:00:18 +0200

    Initial Commit

---
+++ badminton-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  badminton-ios
+//
+//  Created by Gustav Bergström on 2017-06-05.
+//  Copyright © 2017 Gustav Bergström. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
