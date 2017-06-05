e0f2fc824b17ffb3392452b1a5899899a0c4b928
gurrabz <gustav_m_o_bergstrom@hotmail.com>
2017-06-05 23:49:38 +0200

    Add mock players

---
+++ badminton-ios/MockPlayers.swift
@@ -0,0 +1,18 @@
+//
+//  MockPlayers.swift
+//  badminton-ios
+//
+//  Created by Gustav Bergström on 2017-06-05.
+//  Copyright © 2017 Gustav Bergström. All rights reserved.
+//
+
+import Foundation
+
+// Class containing mock players for testing purposes
+
+public class MockPlayers {
+    public static let gustav: Player = Player(firstName: "Gustav", lastName: "Bergström")
+    public static let daniel: Player = Player(firstName: "Daniel", lastName: "Eineving")
+    public static let jesper: Player = Player(firstName: "Jesper", lastName: "Larsson")
+    public static let erik: Player = Player(firstName: "Erik", lastName: "Insgård")
+}
